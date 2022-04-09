f677e2dedb548e9c77887cc4d674c95db6dd6c10
Suru <100470320+SuruXmanager@users.noreply.github.com>
2022-04-09 20:30:58 +0530

    Update welcome.py

--- EmikoRobot/modules/welcome.py
+++ EmikoRobot/modules/welcome.py
@@ -1235,22 +1235,22 @@ def __chat_settings__(chat_id, _):
 
 
 __help__ = """
-*Admins only:*
-❂ /welcome <on/off>*:* enable/disable welcome messages.
-❂ /welcome*:* shows current welcome settings.
-❂ /welcome noformat*:* shows current welcome settings, without the formatting - useful to recycle your welcome messages!
-❂ /goodbye*:* same usage and args as `/welcome`.
-❂ /setwelcome <sometext>*:* set a custom welcome message. If used replying to media, uses that media.
-❂ /setgoodbye <sometext>*:* set a custom goodbye message. If used replying to media, uses that media.
-❂ /resetwelcome*:* reset to the default welcome message.
-❂ /resetgoodbye*:* reset to the default goodbye message.
-❂ /cleanwelcome <on/off>*:* On new member, try to delete the previous welcome message to avoid spamming the chat.
-❂ /welcomemutehelp*:* gives information about welcome mutes.
-❂ /cleanservice <on/off*:* deletes telegrams welcome/left service messages.
+*✘ Admins only ✘:*
+➻ /welcome <on/off>*:* enable/disable welcome messages.
+➻ /welcome*:* shows current welcome settings.
+➻ /welcome noformat*:* shows current welcome settings, without the formatting - useful to recycle your welcome messages!
+➻ /goodbye*:* same usage and args as `/welcome`.
+➻ /setwelcome <sometext>*:* set a custom welcome message. If used replying to media, uses that media.
+➻ /setgoodbye <sometext>*:* set a custom goodbye message. If used replying to media, uses that media.
+➻ /resetwelcome*:* reset to the default welcome message.
+➻ /resetgoodbye*:* reset to the default goodbye message.
+➻ /cleanwelcome <on/off>*:* On new member, try to delete the previous welcome message to avoid spamming the chat.
+➻ /welcomemutehelp*:* gives information about welcome mutes.
+➻ /cleanservice <on/off*:* deletes telegrams welcome/left service messages.
  *Example:*
 user joined chat, user left chat.
-*Welcome markdown:*
-❂ /welcomehelp*:* view more formatting information for custom welcome/goodbye messages.
+*✘ Welcome markdown ✘:*
+➻ /welcomehelp*:* view more formatting information for custom welcome/goodbye messages.
 """
 
 NEW_MEM_HANDLER = MessageHandler(
@@ -1313,7 +1313,7 @@ dispatcher.add_handler(BUTTON_VERIFY_HANDLER)
 dispatcher.add_handler(WELCOME_MUTE_HELP)
 dispatcher.add_handler(CAPTCHA_BUTTON_VERIFY_HANDLER)
 
-__mod_name__ = "Greetings"
+__mod_name__ = "✘ɢʀᴇᴇᴛɪɴɢꜱ✘"
 __command_list__ = []
 __handlers__ = [
     NEW_MEM_HANDLER,
