ac8b6ec0b9d5b45a04489e18b44afc3d792ec9ab
Suru <100470320+SuruXmanager@users.noreply.github.com>
2022-04-09 20:41:02 +0530

    Update disable.py

--- EmikoRobot/modules/disable.py
+++ EmikoRobot/modules/disable.py
@@ -318,13 +318,13 @@ if is_module_loaded(FILENAME):
 
 
     __help__ = """
-❂ /cmds*:* check the current status of disabled commands
-*Admins only:*
-❂ /enable <cmd name>*:* enable that command
-❂ /disable <cmd name>*:* disable that command
-❂ /enablemodule <module name>*:* enable all commands in that module
-❂ /disablemodule <module name>*:* disable all commands in that module
-❂ /listcmds*:* list all possible toggleable commands
+➻ /cmds*:* check the current status of disabled commands
+*✘ Admins only ✘:*
+➻ /enable <cmd name>*:* enable that command
+➻ /disable <cmd name>*:* disable that command
+➻ /enablemodule <module name>*:* enable all commands in that module
+➻ /disablemodule <module name>*:* disable all commands in that module
+➻ /listcmds*:* list all possible toggleable commands
 """
 
     DISABLE_HANDLER = CommandHandler("disable", disable, run_async=True)
@@ -345,7 +345,7 @@ if is_module_loaded(FILENAME):
     dispatcher.add_handler(COMMANDS_HANDLER)
     dispatcher.add_handler(TOGGLE_HANDLER)
 
-    __mod_name__ = "Disabling"
+    __mod_name__ = "✘ᴅɪꜱᴀʙʟɪɴɢ✘"
 
 else:
     DisableAbleCommandHandler = CommandHandler
