23d62557ca16ddf7f7a3497e937cd836ca58aa4a
Suru <100470320+SuruXmanager@users.noreply.github.com>
2022-05-29 20:23:06 +0530

    Update Teddyscript.py

--- EmikoRobot/Teddyscript.py
+++ EmikoRobot/Teddyscript.py
@@ -5,3 +5,12 @@ GROUP_START_IMG = (
     "https://telegra.ph/file/258c02c002e89287d5d9b.jpg",
     "https://telegra.ph/file/9849b3940f063b065f4e3.jpg",
 )
+
+
+TEDDY_DISPACHER_PIC = (
+    "https://telegra.ph/file/dce8565bf7742f3d7122b.jpg",
+    "https://telegra.ph/file/a5e4cb43178642ba3709d.jpg",
+    "https://telegra.ph/file/dc85d43c4fc5062de7274.jpg",
+    "https://telegra.ph/file/89d32e5bbf084a376c803.jpg",
+    "https://telegra.ph/file/47ccb13820d6fc54d872b.jpg",
+)
