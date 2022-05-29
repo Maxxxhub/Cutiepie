5b2a877b0f647270ee17f2bac13c35effc3e7d28
Suru <100470320+SuruXmanager@users.noreply.github.com>
2022-05-29 19:53:46 +0530

    Update __main__.py

--- EmikoRobot/__main__.py
+++ EmikoRobot/__main__.py
@@ -80,7 +80,14 @@ def get_readable_time(seconds: int) -> str:
 
     return ping_time
 
-GROUP_START_IMG = "https://telegra.ph/file/c124da7128cfa1989ca48.jpg"
+GROUP_START_IMG = (
+    "https://telegra.ph/file/c124da7128cfa1989ca48.jpg",
+    "https://telegra.ph/file/5780b3273162d2b9ba9ec.jpg",
+    "https://telegra.ph/file/e2d56d5dbb108ba7af20c.jpg",
+    "https://telegra.ph/file/258c02c002e89287d5d9b.jpg",
+    "https://telegra.ph/file/9849b3940f063b065f4e3.jpg",
+)
+    
 
 
 PM_START_TEXT = """
