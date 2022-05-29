9347a99190b0e914fd287308cb69c46a6bfe7c2d
Suru <100470320+SuruXmanager@users.noreply.github.com>
2022-05-29 20:45:30 +0530

    Delete script.py

--- EmikoRobot/script.py
+++
@@ -1,7 +0,0 @@
-GROUP_START_IMG = (
-    "https://telegra.ph/file/c124da7128cfa1989ca48.jpg",
-    "https://telegra.ph/file/5780b3273162d2b9ba9ec.jpg",
-    "https://telegra.ph/file/e2d56d5dbb108ba7af20c.jpg",
-    "https://telegra.ph/file/258c02c002e89287d5d9b.jpg",
-    "https://telegra.ph/file/9849b3940f063b065f4e3.jpg",
-)
