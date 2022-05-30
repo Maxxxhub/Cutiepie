8ed3638edd24116b9dc42b7e5ab2b0ac6b46eee7
Suru <100470320+SuruXmanager@users.noreply.github.com>
2022-05-30 13:00:33 +0530

    Create script.py

---
+++ EmikoRobot/script.py
@@ -0,0 +1,7 @@
+TEDDY_START_IMG = (
+  "https://telegra.ph/file/dc85d43c4fc5062de7274.jpg",
+  "https://telegra.ph/file/dce8565bf7742f3d7122b.jpg",
+  "https://telegra.ph/file/78ffc400d4f3236b00e6b.jpg",
+  "https://telegra.ph/file/3f97672eb7b50426d15ff.jpg",
+  "https://telegra.ph/file/a5e4cb43178642ba3709d.jpg",
+)
