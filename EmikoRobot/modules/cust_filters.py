6507e705ed819069d137047aa7c48028ba33ccdb
Suru <100470320+SuruXmanager@users.noreply.github.com>
2022-06-07 19:07:00 +0530

    Update cust_filters.py

--- EmikoRobot/modules/cust_filters.py
+++ EmikoRobot/modules/cust_filters.py
@@ -628,7 +628,7 @@ doin?
 Check /markdownhelp to know more!
 """
 
-__mod_name__ = "✘ꜰɪʟᴛᴇʀꜱ✘"
+__mod_name__ = "Fɪʟᴛᴇʀꜱ"
 
 FILTER_HANDLER = CommandHandler("filter", filters)
 STOP_HANDLER = CommandHandler("stop", stop_filter)
