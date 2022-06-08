d9f739202c3585b17830b2a03072fc6561c0bb8a
Suru <100470320+SuruXmanager@users.noreply.github.com>
2022-06-08 20:27:26 +0530

    Update alive.py

--- EmikoRobot/modules/alive.py
+++ EmikoRobot/modules/alive.py
@@ -8,15 +8,15 @@ from pyrogram import __version__ as pyrover
 from EmikoRobot.events import register
 from EmikoRobot import telethn as tbot
 
-
+PHOTO = "https://te.legra.ph/file/249fb93bb4219d5a0368f.jpg"
 @register(pattern=("/alive"))
 async def awake(event):
   TEXT = f"**ʜɪɪ [{event.sender.first_name}](tg://user?id={event.sender.id}), ɪ'ᴍ ᴛᴇᴅᴅy ʀᴏʙᴏᴛ.** \n"
   TEXT += "⚪ **ɪ'ᴍ ᴡᴏʀᴋɪɴɢ ᴩʀᴏᴩᴇʀʟy** \n"
   TEXT += f"⚪ **ᴍy ᴍᴀꜱᴛᴇʀ : [ꜱᴜʀᴜ](https://t.me/smokerr_xd)** \n"
-  TEXT += f"⚪ **ʟɪʙʀᴀʀy ᴠᴇʀꜱɪᴏɴ :** `{telever}` \n"
+  TEXT += f"⚪ <b>ʟɪʙʀᴀʀy ᴠᴇʀꜱɪᴏɴ :</b> `{telever}` \n"
   TEXT += f"⚪ **ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀꜱɪᴏɴ :** `{tlhver}` \n"
   TEXT += f"⚪ **ᴩyʀᴏɢʀᴀᴍ ᴠᴇʀꜱɪᴏɴ :** `{pyrover}` \n"
   TEXT += "**ᴛʜᴀɴᴋꜱ ꜰᴏʀ ᴀᴅᴅɪɴɢ ᴍᴇ ʜᴇʀᴇ, ᴅᴀʀʟɪɴɢ❤️**"
-  BUTTON = [[Button.url("♡ ʜᴇʟᴩ ♡", "https://t.me/Teddyrobot_bot?start=help"), Button.url("♡ ꜱᴜᴩᴩᴏʀᴛ ♡", "https://t.me/Teddysupport")]]
+  BUTTON = [[Button.url(" ♡ Hᴇʟᴩ ♡", "https://t.me/Teddyrobot_bot?start=help"), Button.url("♡ Sᴜᴩᴩᴏʀᴛ ♡", "https://t.me/teddysupport")]]
   await tbot.send_file(event.chat_id, PHOTO, caption=TEXT,  buttons=BUTTON)
