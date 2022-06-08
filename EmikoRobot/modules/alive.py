5ad7cead3e4d3e53c3d2cedcc97862f0a34c5fda
Suru <100470320+SuruXmanager@users.noreply.github.com>
2022-06-08 20:01:45 +0530

    Update alive.py

--- EmikoRobot/modules/alive.py
+++ EmikoRobot/modules/alive.py
@@ -9,16 +9,31 @@ from EmikoRobot.events import register
 from EmikoRobot import telethn as tbot
 
 
-PHOTO = "https://telegra.ph/file/2c13e570944404a6c0d86.jpg"
-
 @register(pattern=("/alive"))
 async def awake(event):
-  TEXT = f"**ʜɪɪ [{event.sender.first_name}](tg://user?id={event.sender.id}), ɪ'ᴍ ᴛᴇᴅᴅy ʀᴏʙᴏᴛ.** \n\n"
-  TEXT += "⚪ **ɪ'ᴍ ᴡᴏʀᴋɪɴɢ ᴩʀᴏᴩᴇʀʟy** \n\n"
-  TEXT += f"⚪ **ᴍy ᴍᴀꜱᴛᴇʀ : [ꜱᴜʀᴜ](https://t.me/smokerr_xd)** \n\n"
-  TEXT += f"⚪ **ʟɪʙʀᴀʀy ᴠᴇʀꜱɪᴏɴ :** `{telever}` \n\n"
-  TEXT += f"⚪ **ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀꜱɪᴏɴ :** `{tlhver}` \n\n"
-  TEXT += f"⚪ **ᴩyʀᴏɢʀᴀᴍ ᴠᴇʀꜱɪᴏɴ :** `{pyrover}` \n\n"
+  TEXT = f"**ʜɪɪ [{event.sender.first_name}](tg://user?id={event.sender.id}), ɪ'ᴍ ᴛᴇᴅᴅy ʀᴏʙᴏᴛ.** \n"
+  TEXT += "⚪ **ɪ'ᴍ ᴡᴏʀᴋɪɴɢ ᴩʀᴏᴩᴇʀʟy** \n"
+  TEXT += f"⚪ **ᴍy ᴍᴀꜱᴛᴇʀ : [ꜱᴜʀᴜ](https://t.me/smokerr_xd)** \n"
+  TEXT += f"⚪ **ʟɪʙʀᴀʀy ᴠᴇʀꜱɪᴏɴ :** `{telever}` \n"
+  TEXT += f"⚪ **ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀꜱɪᴏɴ :** `{tlhver}` \n"
+  TEXT += f"⚪ **ᴩyʀᴏɢʀᴀᴍ ᴠᴇʀꜱɪᴏɴ :** `{pyrover}` \n"
   TEXT += "**ᴛʜᴀɴᴋꜱ ꜰᴏʀ ᴀᴅᴅɪɴɢ ᴍᴇ ʜᴇʀᴇ, ᴅᴀʀʟɪɴɢ❤️**"
-  BUTTON = [[Button.url("Hʜᴇʟᴩ", "https://t.me/Teddyrobot_bot?start=help"), Button.url("ꜱᴜᴩᴩᴏʀᴛ", "https://t.me/teddysupport")]]
+  
+else:
+        update.effective_message.reply_photo(
+            random.choice(TEDDY_GROUP_START_IMG)
+            ),
+            parse_mode=ParseMode.HTML,
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton(
+                            text="♠️ Hʜᴇʟᴩ ♠️",
+                            url="https://t.me/Teddyrobot_bot?start=help",
+                          InlineKeyboardButton(text="♠️ ꜱᴜᴩᴩᴏʀᴛ ♠️", url="https://t.me/teddysupport"),
+                        ),
+                    ]
+                ]
+            ),
+        )
   await tbot.send_file(event.chat_id, PHOTO, caption=TEXT,  buttons=BUTTON)
