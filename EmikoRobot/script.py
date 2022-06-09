732c66b9f7bbdce2683bbc6a036830a6187a7d62
Suru <100470320+SuruXmanager@users.noreply.github.com>
2022-06-09 06:59:08 +0530

    Update script.py

--- EmikoRobot/script.py
+++ EmikoRobot/script.py
@@ -13,5 +13,11 @@ TEDDY_GROUP_START_IMG = (
   "https://telegra.ph/file/8a14ecd2347ef88e81201.jpg",
   "https://telegra.ph/file/3a88a958a88e83d85d284.jpg",
   "https://telegra.ph/file/14be160df3b84c59e268e.jpg",
+  "https://telegra.ph/file/e6638aaabc97759b38fa2.jpg",
+"https://telegra.ph/file/71ee1cad03e0670774b05.jpg",
+"https://telegra.ph/file/075bc9f1ee33f3e7068d9.jpg",
+"https://telegra.ph/file/b1ce8e4af8b7109f62553.jpg",
+"https://telegra.ph/file/502ff150e26e9f5aa9bdd.jpg",
+"https://telegra.ph/file/64f01f5b87e7963d1a79e.jpg",
+"https://telegra.ph/file/acdef9f9c0e24ad4f019e.jpg",
 )
-  
