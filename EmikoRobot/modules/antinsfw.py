283f02bb1dfeee6a3b6b22f101cea5cb3672d9b5
Suru <107604575+Suruxhub@users.noreply.github.com>
2022-06-21 20:00:15 +0530

    Update antinsfw.py

--- EmikoRobot/modules/antinsfw.py
+++ EmikoRobot/modules/antinsfw.py
@@ -81,16 +81,16 @@ async def detect_nsfw(_, message):
         return
     await message.reply_text(
         f"""
-**NSFW Image Detected & Deleted Successfully!
+**NSFW Iᴍᴀɢᴇ ᴅᴇᴛᴇᴄᴛᴇᴅ ᴀɴᴅ ᴅᴇʟᴇᴛᴇᴅ sᴜᴄᴄᴇssғᴜʟʟʏ!
 ————————————————————**
-**User:** {message.from_user.mention} [`{message.from_user.id}`]
-**Safe:** `{results.neutral} %`
-**Porn:** `{results.porn} %`
-**Adult:** `{results.sexy} %`
-**Hentai:** `{results.hentai} %`
-**Drawings:** `{results.drawings} %`
+**Usᴇʀ:** {message.from_user.mention} [`{message.from_user.id}`]
+**Sᴀғᴇ:** `{results.neutral} %`
+**Pᴏʀɴ:** `{results.porn} %`
+**Aᴅᴜʟᴛ:** `{results.sexy} %`
+**Hᴇɴᴛᴀɪ:** `{results.hentai} %`
+**Dʀᴀᴡɪɴɢs:** `{results.drawings} %`
 **————————————————————**
-__Use `/antinsfw off` to disable this.__
+__Usᴇ `/antinsfw off` ᴛᴏ ᴅɪsᴀʙʟᴇ ᴛʜɪs.__
 """
     )
 
@@ -100,7 +100,7 @@ __Use `/antinsfw off` to disable this.__
 async def nsfw_scan_command(_, message):
     if not message.reply_to_message:
         await message.reply_text(
-            "`Reply to an image/document/sticker/animation to scan it.`"
+            "`Rᴇᴘʟʏ ᴛᴏ ᴀɴ ɪᴍᴀɢᴇ/ᴅᴏᴄᴜᴍᴇɴᴛ/sᴛɪᴄᴋᴇʀ/ᴀɴɪᴍᴀᴛɪᴏɴ ᴛᴏ sᴄᴀᴍ ɪᴛ.`"
         )
         return
     reply = message.reply_to_message
@@ -112,13 +112,13 @@ async def nsfw_scan_command(_, message):
         and not reply.video
     ):
         await message.reply_text(
-            "Reply to an image/document/sticker/animation to scan it."
+            "Rᴇᴘʟʏ ᴛᴏ ᴀɴ ɪᴍᴀɢᴇ/ᴅᴏᴄᴜᴍᴇɴᴛ/sᴛɪᴄᴋᴇʀ/ᴀɴɪᴍᴀᴛɪᴏɴ ᴛᴏ sᴄᴀɴ ɪᴛ."
         )
         return
-    m = await message.reply_text("`Scanning...`")
+    m = await message.reply_text("`Sᴄᴀɴɴɪɴɢ...`")
     file_id = await get_file_id_from_message(reply)
     if not file_id:
-        return await m.edit("`Something wrong happened...|")
+        return await m.edit("`Sᴏᴍᴇᴛʜɪɴɢ ᴡʀᴏᴍɢ ʜᴀᴘᴘᴇɴᴇᴅ...|")
     file = await pbot.download_media(file_id)
     try:
         results = await arq.nsfw_scan(file=file)
@@ -144,7 +144,7 @@ async def nsfw_scan_command(_, message):
 @adminsOnly("can_change_info")
 async def nsfw_enable_disable(_, message):
     if len(message.command) != 2:
-        await message.reply_text("Usage: /antinsfw [on/off]")
+        await message.reply_text("Usᴀɢᴇ: /antinsfw [on/off]")
         return
     status = message.text.split(None, 1)[1].strip()
     status = status.lower()
@@ -152,10 +152,10 @@ async def nsfw_enable_disable(_, message):
     if status == "on" or status == "yes":
         await nsfw_on(chat_id)
         await message.reply_text(
-            "Enabled AntiNSFW System. I will Delete Messages Containing Inappropriate Content."
+            "Eɴᴀʙᴋᴇᴅ ᴀɴᴛɪNSFW sʏsᴛᴇᴍ. I ᴡɪʟʟ ᴅᴇʟᴇᴛᴇ ᴍsɢ's' ᴄᴏɴᴛᴀɪɴɪɴɢ ɪɴᴀᴘᴘʀᴏᴘʀɪᴀᴛᴇ ᴄᴏɴᴛᴇɴᴛ."
         )
     elif status == "off" or status == "no":
         await nsfw_off(chat_id)
-        await message.reply_text("Disabled AntiNSFW System.")
+        await message.reply_text("Dɪsᴀʙʟᴇᴅ ᴀɴᴛɪNSFW sʏsᴛᴇᴍ.")
     else:
-        await message.reply_text("Unknown Suffix, Use /antinsfw [on/off]")
+        await message.reply_text("Uɴᴋɴᴏᴡɴ sᴜғᴅɪx, Usᴇ /antinsfw [on/off]")
