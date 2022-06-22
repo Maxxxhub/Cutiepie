6f1d34cabf6fbc1d853dbe486122301f2c4f2c9b
Suru <107604575+Suruxhub@users.noreply.github.com>
2022-06-22 11:04:07 +0530

    Create cleanservice.py

---
+++ EmikoRobot/modules/cleanservice.py
@@ -0,0 +1,16 @@
+__help__ = """
+➻ /antiservice <on / off>
+Note: If on, The bot will auto delete service messages for ex: user joined , user left etc...
+"""
+
+CLEAN_SERVICE_HANDLER = CommandHandler(
+    "cleanservice", cleanservice, filters=Filters.chat_type.groups, run_async=True
+  
+  
+  dispatcher.add_handler(CLEAN_SERVICE_HANDLER)
+  
+  __mod_name__ = "A-sᴇʀᴠɪᴄᴇ"
+  __command_list__ = []
+  __handlers__ = [
+    CLEAN_SERVICE_HANDLER
+  ]
