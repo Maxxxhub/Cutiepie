6d7fa7354ec363477c11fc0dee431bac2dd73b17
Suru <107604575+Suruxhub@users.noreply.github.com>
2022-06-22 08:51:59 +0530

    Update tools.py

--- EmikoRobot/modules/tools.py
+++ EmikoRobot/modules/tools.py
@@ -13,19 +13,7 @@ __help__ = """
 ➻ /decrypt*:* Decrypts Previously Ecrypted Text
 ➻ /zip*:* reply to a telegram file to compress it in .zip format
 ➻ /unzip*:* reply to a telegram file to decompress it from the .zip format
-*✘ Random API ✘*
-➻ /ptl or /asupan: get random video from Instagram timeline
-➻ /chika: get random video from chikakiku
-➻ /wibu:get random short anime video or photos
-➻ /truth: challenge 
-➻ /dare: challenge
-✘ Link To File ✘:
-❂ /up: reply to a direct download link to upload it to telegram as files
- 
- ✘ File To Link ✘:
-➻ /transfersh: reply to a telegram file to upload it on transfersh and get direct download link
-➻ /tmpninja: reply to a telegram file to upload it on tmpninja and get direct download link
- ✘ File Or Text To Telegraph ✘:
+ ✘ Telegraph ✘:
 ➻ /tgm : Get Telegraph Link Of Replied Media
 ➻ /tgt: Get Telegraph Link of Replied Text
  ✘ Tagger ✘
