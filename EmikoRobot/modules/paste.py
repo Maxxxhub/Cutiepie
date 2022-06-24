9404887272dc910fdfdae813b64f286700264e29
Suru <107604575+Suruxhub@users.noreply.github.com>
2022-06-24 13:49:10 +0530

    Update paste.py

--- EmikoRobot/modules/paste.py
+++ EmikoRobot/modules/paste.py
@@ -39,19 +39,19 @@ async def isPreviewUp(preview: str) -> bool:
 async def paste_func(_, message):
     if not message.reply_to_message:
         return await message.reply_text(
-            "Reply To A Message With /paste"
+            "Rᴇᴘʟʏ ᴛᴏ ᴀ ᴍsɢ ᴡɪᴛʜ /paste"
         )
-    m = await message.reply_text("Pasting...")
+    m = await message.reply_text("Pᴀsᴛɪɴɢ...")
     if message.reply_to_message.text:
         content = str(message.reply_to_message.text)
     elif message.reply_to_message.document:
         document = message.reply_to_message.document
         if document.file_size > 1048576:
             return await m.edit(
-                "You can only paste files smaller than 1MB."
+                "Yᴏᴜ ᴄᴀɴ ᴏɴʟʏ ᴘᴀsᴛᴇ ғɪʟᴇs sᴍᴀʟʟᴇʀ ᴛʜᴀɴ 1MB."
             )
         if not pattern.search(document.mime_type):
-            return await m.edit("Only text files can be pasted.")
+            return await m.edit("Oɴʟʏ ᴛᴇxᴛ ғɪʟᴇs ᴄᴀɴ ʙᴇ ᴘᴀsᴛᴇᴅ.")
         doc = await message.reply_to_message.download()
         async with aiofiles.open(doc, mode="r") as f:
             content = await f.read()
@@ -59,7 +59,7 @@ async def paste_func(_, message):
     link = await paste(content)
     preview = link + "/preview.png"
     button = InlineKeyboard(row_width=1)
-    button.add(InlineKeyboardButton(text="Paste Link", url=link))
+    button.add(InlineKeyboardButton(text="🔗 Pᴀsᴛᴇ ʟɪɴᴋ 🔗", url=link))
 
     if await isPreviewUp(preview):
         try:
