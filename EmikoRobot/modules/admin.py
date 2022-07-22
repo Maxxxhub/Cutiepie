4c55840a4d698811baaeb1191c3a81b65a394199
Suru <107604575+Suruxhub@users.noreply.github.com>
2022-07-22 08:57:03 +0530

    Update admin.py

--- EmikoRobot/modules/admin.py
+++ EmikoRobot/modules/admin.py
@@ -966,36 +966,31 @@ def bug_reporting(update: Update, _: CallbackContext):
         pass
 
 
-__help__ = """
-*User Commands:*
-➻ /admins*:* list of admins in the chat
-➻ /pinned*:* to get the current pinned message.
-*The Following Commands are Admins only:*
-➻ /pin*:* silently pins the message replied to - add `'loud'` or `'notify'` to give notifs to users
+__help__ =  """
+*Usᴇʀ ᴄᴏᴍᴍᴀɴᴅs:*
+➻ /admins*:* Lɪsᴛ ᴏғ ᴀᴅᴍɪɴs.
+➻ /pinned*:* Tᴏ ɢᴇᴛ ᴄᴜʀʀᴇɴᴛ ᴘɪɴɴᴇᴅ ᴍᴇssᴀɢᴇ.
+*Aᴅᴍɪɴs ᴏɴʟʏ ᴄᴏᴍᴍᴀɴᴅs:*
+➻ /pin*:* Sɪʟᴇɴᴛʟʏ ᴘɪɴ ᴛʜᴇ ᴍᴇssᴀɢᴇ ʀᴇᴘʟɪᴇᴅ ᴛᴏ - ᴀᴅᴅ `'loud'` ᴏʀ `'notify'` ᴛᴏ ɢɪᴠᴇ ɴᴏᴛɪғs ᴛᴏ ᴜsᴇʀ$/ᴍᴇᴍʙᴇʀs.
 ➻ /unpin*:* unpins the currently pinned message
-➻ /invitelink*:* gets invitelink
-➻ /promote*:* promotes the user replied to
-➻ /fullpromote*:* promotes the user replied to with full rights
-➻ /demote*:* demotes the user replied to
-➻ /title <title here>*:* sets a custom title for an admin that the bot promoted
-➻ /admincache*:* force refresh the admins list
-➻ /del*:* deletes the message you replied to
-➻ /purge*:* deletes all messages between this and the replied to message.
-➻ /purge <integer X>*:* deletes the replied message, and X messages following it if replied to a message.
-➻ /setgtitle <text>*:* set group title
-➻ /setgpic*:* reply to an image to set as group photo
-➻ /setdesc*:* Set group description
-➻ /setsticker*:* Set group sticker
-➻ /bug*:* to report a bug on bot \n• example: /bug Music not working.
-*Rules:*
-➻ /rules*:* get the rules for this chat.
-➻ /setrules <your rules here>*:* set the rules for this chat.
-➻ /clearrules*:* clear the rules for this chat.
-*The Following Commands are stuff:*
-➻ /myinfo*:* try it urself bruh.
-➻ /ginfo <group username>*:* for group info which are in teddy's db.
-➻ /cinfo <channel username>*:* for channel info which are in teddy's db.
-➻ /uinfo*:* try it urself bruh.
+➻ /invitelink*:* Gᴇᴛs ɪɴᴠɪᴛᴇʟɪɴᴋ.
+➻ /promote*:* Pʀᴏᴍᴏᴛᴇs ᴛʜᴇ ᴜsᴇʀ ʀᴇᴘʟɪᴇᴅ ᴛᴏ
+
+➻ /fullpromote*:* Pʀᴏᴍᴏᴛᴇs ᴛʜᴇ ᴜsᴇʀ ʀᴇᴘʟɪᴇᴅ ᴛᴏ ᴡɪᴛʜ ғᴜʟʟ ᴀᴅᴍɪɴ ʀɪɢʜᴛs.
+➻ /demote*:* Dᴇᴍᴏᴛᴇs ᴛʜᴇ ᴜsᴇʀ ʀᴇᴘʟɪᴇᴅ ᴛᴏ.
+➻ /title <title here>*:* Sᴇᴛs ᴀ ᴄᴜsᴛᴏᴍ ᴛɪᴛʟᴇ ғᴏʀ ᴀɴ ᴀᴅᴍɪɴ ᴡʜᴏ ɢᴏᴛ ᴘʀᴏᴍᴏᴛᴇᴅ ʙʏ ᴛᴇᴅᴅʏ ʙᴏᴛ.
+➻ /admincache*:* Rᴇғʀᴇsʜ ᴛʜᴇ ᴀᴅᴍɪɴ ʟɪsᴛ
+➻ /del*:* Dᴇʟᴇᴛᴇs ᴛʜᴇ ᴍᴇssᴀɢᴇ ʏᴏᴜ ʀᴇᴘʟɪᴇᴅ ᴛᴏ.
+➻ /purge*:* Dᴇʟᴇᴛᴇs ᴀʟʟ ᴍᴇssᴀɢᴇs ʙᴇᴛᴡᴇᴇɴ ᴛʜɪs ᴀɴᴅ ᴛʜᴇ ʀᴇᴘʟɪᴇᴅ ᴛᴏ ᴍᴇssᴀɢᴇ.
+➻ /purge <integer X>*:* Dᴇʟᴇᴛᴇs ᴛʜᴇ ʀᴇᴘʟɪᴇᴅ ᴍᴇssᴀɢᴇ, ᴀɴᴅ X ᴍᴇssᴀɢᴇs ғᴏʟʟᴏᴡɪɴɢ ɪᴛ.
+➻ /setgtitle <text>*:* Sᴇᴛ ɢʀᴏᴜᴘ ᴛɪᴛʟᴇ.
+➻ /setgpic*:* Rᴇᴘʟʏ ᴛᴏ ᴀɴ ɪᴍᴀɢᴇ ᴛᴏ sᴇᴛ ɪᴛ ᴀs ᴀ ɢʀᴏᴜᴘ ᴘғᴘ.
+➻ /setdesc*:* Sᴇᴛ ɢʀᴏᴜᴘ ᴅᴇsᴄʀɪᴘᴛɪᴏɴ.
+➻ /setsticker*:* Sᴇᴛ ɢʀᴏᴜᴘ sᴛɪᴄʟᴇʀ.
+*Rᴜʟᴇs:*
+➻ /rules*:* Tᴏ ɢᴇᴛ ᴛʜᴇ ʀᴜʟᴇs.
+➻ /setrules <your rules here>*:* Tᴏ sᴇᴛ ᴛʜᴇ ʀᴜʟᴇs ғᴏʀ ʏᴏᴜʀ ᴄʜᴀᴛ.
+➻ /clearrules*:* Tᴏ ᴄʟᴇᴀʀ ᴛʜᴇ ʀᴜʟᴇs.
 """
 
 SET_DESC_HANDLER = CommandHandler("setdesc", set_desc, filters=Filters.chat_type.groups, run_async=True)
