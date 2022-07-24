1770dac154f80f0b01a3074a7c6b78d3e57bcb74
Suru <107604575+Suruxhub@users.noreply.github.com>
2022-07-24 20:34:10 +0530

    Tᴇᴅᴅʏ#
    Nᴛɢ

--- EmikoRobot/modules/musichelp.py
+++ EmikoRobot/modules/musichelp.py
@@ -1,15 +1,12 @@
 __mod_name__ = "Mᴜꜱɪᴄ ᴩʟᴀyᴇʀ"
 
 __help__ = """
-──「 Music stream 」──              
-➻ /play (query or reply audio) to playing music from yourube or your audio telegram.
-➻ /playlist (choose option) for playing your personal playlist or group playlist.
-✘ Admins only ✘
-➻ /pause  - Pause the playing music.
-➻ /resume - Resume the paused music.
-➻ /mute - Mute the playing music.
-➻ /unmute - Unmute the muted music.
-➻ /skip - Skip the current playing music.
-➻ /vol (range 1-200) - To set volume on voice calls.
-NOTE : DON'T SPAM TO REQUEST A SONG OR VIDEO...
+──「 Mᴜsɪᴄ Sᴛʀᴇᴀᴍ 」──              
+➻ /play (ǫᴜᴇʀʏ ᴏʀ ʀᴇᴘʟʏ ᴛᴏ ᴀᴜᴅɪᴏ) Tᴏ ᴘʟᴀʏ ᴍᴜsɪᴄ ᴏɴ ᴠᴄ ғʀᴏᴍ ʏᴏᴜᴛᴜʙᴇ ᴏʀ ʏᴏᴜʀ ʀᴇᴘʟɪᴇᴅ ᴛᴇʟᴇɢʀᴀᴍ ᴀᴜᴅɪᴏ.
+➻ /playlist (ᴄʜᴏᴏsᴇ ᴏᴘᴛɪᴏɴ) Tᴏ ᴘʟᴀʏ ʏᴏᴜʀ ᴘᴇʀsᴏɴᴀʟ ᴘʟᴀʏʟɪsᴛ ᴏʀ ɢʀᴏᴜᴘ ᴘʟᴀʏʟɪsᴛ.
+✘ Aᴅᴍɪɴ/- ✘
+➻ /pause  - Pᴀᴜsᴇ ᴛʜᴇ ᴘʟᴀʏɪɴɢ ᴛʀᴀᴄᴋ.
+➻ /resume - Rᴇsᴜᴍᴇ ᴛʜᴇ ᴘᴀᴜsᴇᴅ ᴛʀᴀᴄᴋ.
+➻ /skip - Sᴋɪᴘ ᴛʜᴇ ᴄᴜʀʀᴇɴᴛ ᴘʟᴀʏɪɴɢ sᴏɴɢ.
+NOTE : DON'T SPAM TO REQUEST A SONG OR VIDEO...SPAM KAROGE TO CHAPPAL BHI PAD SAKTI HAI..WE R NOT JIMEDR FOR THAT ✌️
 """
