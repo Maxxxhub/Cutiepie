540e41504154a4768595eef5651cf321dcc7bab5
Suru <107604575+Suruxhub@users.noreply.github.com>
2022-07-25 09:55:03 +0530

    Create afkhelper.py

---
+++ EmikoRobot/helper_extra/afkhelper.py
@@ -0,0 +1,31 @@
+import asyncio
+
+from typing import Union
+from datetime import datetime, timedelta
+from Yukki import cleanmode, app, botname
+from Yukki.database import is_cleanmode_on
+from pyrogram.errors import FloodWait
+
+
+def get_readable_time(seconds: int) -> str:
+    count = 0
+    ping_time = ""
+    time_list = []
+    time_suffix_list = ["s", "m", "h", "days"]
+    while count < 4:
+        count += 1
+        if count < 3:
+            remainder, result = divmod(seconds, 60)
+        else:
+            remainder, result = divmod(seconds, 24)
+        if seconds == 0 and remainder == 0:
+            break
+        time_list.append(int(result))
+        seconds = int(remainder)
+    for i in range(len(time_list)):
+        time_list[i] = str(time_list[i]) + time_suffix_list[i]
+    if len(time_list) == 4:
+        ping_time += time_list.pop() + ", "
+    time_list.reverse()
+    ping_time += ":".join(time_list)
+    return ping_time
