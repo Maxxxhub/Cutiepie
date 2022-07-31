e01259dd85c7c494f8703ef7f9c5422464b4b777
Suru <107604575+Suruxhub@users.noreply.github.com>
2022-07-31 20:10:13 +0530

    Create stylefont.py

---
+++ EmikoRobot/modules/stylefont.py
@@ -0,0 +1,13 @@
+__mod_name__ = "Fᴏɴᴛ"
+
+__help__ = """
+*Sᴛʏʟɪsʜ ᴛᴇxᴛ*
+➻ /weebify <text>: Wᴇᴇʙɪғʏ ʏᴏᴜʀ ᴛᴇxᴛ!
+➻ /bubble <text>:Bᴜʙʙʟᴇ ʏᴏᴜʀ ᴛᴇxᴛ!
+➻ /fbubble <text>: Bᴜʙʙʟᴇ-ғɪʟʟᴇᴅ ʏᴏᴜʀ ᴛᴇxᴛ
+➻ /square <text>: Sǫᴜᴀʀᴇ ʏᴏᴜʀ ᴛᴇxᴛ!
+➻ /fsquare <text>: Sǫᴜᴀʀᴇ-ғɪʟʟᴇᴅ ʏᴏᴜʀ ᴛᴇxᴛ!
+➻ /blue <text>: Bʟᴜɪғʏ ʏᴏᴜʀ ᴛᴇxᴛ!
+➻ /latin <text>: Lᴀᴛɪɴɪғʏ ʏᴏᴜʀ ᴛᴇxᴛ
+➻ /lined <text>: Lɪɴᴇᴅ ʏᴏᴜʀ ᴛᴇxᴛ
+"""
