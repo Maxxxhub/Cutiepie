d04819a6033981f8008a8ae4848b17e51884bf79
Suru <107604575+Suruxhub@users.noreply.github.com>
2022-08-02 15:53:33 +0530

    Update misc.py

--- EmikoRobot/modules/helper_funcs/misc.py
+++ EmikoRobot/modules/helper_funcs/misc.py
@@ -1,4 +1,5 @@
 from typing import Dict, List
+from math import ceil
 
 from EmikoRobot import NO_LOAD
 from telegram import MAX_MESSAGE_LENGTH, Bot, InlineKeyboardButton, ParseMode
@@ -68,11 +69,26 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
     calc = len(modules) - round(round_num)
     if calc in [1, 2]:
         pairs.append((modules[-1],))
-    elif calc == 2:
-        pairs.append((modules[-1],))
+
+    max_num_pages = ceil(len(pairs) / 5)
+    modulo_page = page_n % max_num_pages
+
+    # can only have a certain amount of buttons side by side
+    if len(pairs) > 3:
+        pairs = pairs[modulo_page * 5 : 5 * (modulo_page + 1)] + [
+            (
+                EqInlineKeyboardButton(
+                    "◁", callback_data="{}_prev({})".format(prefix, modulo_page)
+                ),
+                EqInlineKeyboardButton(" Hᴏᴍᴇ ", callback_data="emiko_back"),
+                EqInlineKeyboardButton(
+                    "▷", callback_data="{}_next({})".format(prefix, modulo_page)
+                ),
+            )
+        ]
 
     else:
-        pairs += [[EqInlineKeyboardButton("Bᴀᴄᴋ ❗", callback_data="emiko_back")]]
+        pairs += [[EqInlineKeyboardButton("• ʙᴀᴄᴋ •", callback_data="emiko_back")]]
 
     return pairs
 
