917ce8216d58f595f395c7d90958893699a9fa5d
Suru <107604575+Suruxhub@users.noreply.github.com>
2022-08-03 09:15:27 +0530

    Update misc.py

--- EmikoRobot/modules/helper_funcs/misc.py
+++ EmikoRobot/modules/helper_funcs/misc.py
@@ -70,12 +70,12 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
     if calc in [1, 2]:
         pairs.append((modules[-1],))
 
-    max_num_pages = ceil(len(pairs) / 5)
+    max_num_pages = ceil(len(pairs) / 7)
     modulo_page = page_n % max_num_pages
 
     # can only have a certain amount of buttons side by side
     if len(pairs) > 2:
-        pairs = pairs[modulo_page * 5 : 5 * (modulo_page + 1)] + [
+        pairs = pairs[modulo_page * 7 : 7 * (modulo_page + 1)] + [
             (
                 EqInlineKeyboardButton(
                     "◁", callback_data="{}_prev({})".format(prefix, modulo_page)
