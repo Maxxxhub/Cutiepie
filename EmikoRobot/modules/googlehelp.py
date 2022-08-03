f83dfde5f24816f933d7c7c0c91751d913b45c85
Suru <107604575+Suruxhub@users.noreply.github.com>
2022-08-03 11:28:07 +0530

    Create googlehelp.py

---
+++ EmikoRobot/modules/googlehelp.py
@@ -0,0 +1,9 @@
+__help__ = """
+× /google <text>: Perform a google search nd returns.
+ × /img <text>: Search Google for images and returns them
+For greater no. of results specify lim, For eg: /img hello lim=10
+ × /app <appname>: Searches for an app in Play Store and returns its details.
+ × /reverse: Does a reverse image search of the media which it was replied to.
+ """
+
+__mod_name__ = "Google"
