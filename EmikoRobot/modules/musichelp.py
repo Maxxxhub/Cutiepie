e472b29306de54ffa8e7baf8217c40fce30d738d
Suru <107604575+Suruxhub@users.noreply.github.com>
2022-08-03 16:58:02 +0530

    Delete musichelp.py

--- EmikoRobot/modules/musichelp.py
+++
@@ -1,7 +0,0 @@
-__help__ = """
-× /song <song name>: Uploads the song in it's best quality available on YouTube.
-× /video <song/video name>! Uploads video in it's best quality available on YouTube.
- 💡Ex: `/song often remix`
- 💡Ex: `/video in the end remix` """
-
-__mod_name__ = "Music"
