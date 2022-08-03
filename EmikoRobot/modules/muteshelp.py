7d8e622d7b895ddbaed1c5127e26985ff43402f5
Suru <107604575+Suruxhub@users.noreply.github.com>
2022-08-03 10:04:50 +0530

    Create muteshelp.py

---
+++ EmikoRobot/modules/muteshelp.py
@@ -0,0 +1,7 @@
+__help__ = """
+➻ /mute <userhandle>*:* Sɪʟᴇɴᴄᴇs ᴀ ᴜsᴇʀ.
+➻ /tmute <userhandle> X(m/h/d)*:* Mᴜᴛᴇs ᴀ ᴜsᴇʀ ғᴏʀ X ᴛɪᴍᴇ. m = minutes, h = hours, d = days.
+➻ /unmute <userhandle>*:* Uɴᴍᴜᴛᴇs ᴀ ᴜsᴇʀ.
+"""
+
+__mod_name__ = "Mᴜᴛᴇs"
