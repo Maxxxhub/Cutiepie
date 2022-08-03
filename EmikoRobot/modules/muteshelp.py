13c641503f28782c8878f5a1f4021cb0cd093868
Suru <107604575+Suruxhub@users.noreply.github.com>
2022-08-03 17:02:38 +0530

    Delete muteshelp.py

--- EmikoRobot/modules/muteshelp.py
+++
@@ -1,7 +0,0 @@
-__help__ = """
-➻ /mute <userhandle>*:* Sɪʟᴇɴᴄᴇs ᴀ ᴜsᴇʀ.
-➻ /tmute <userhandle> X(m/h/d)*:* Mᴜᴛᴇs ᴀ ᴜsᴇʀ ғᴏʀ X ᴛɪᴍᴇ. m = minutes, h = hours, d = days.
-➻ /unmute <userhandle>*:* Uɴᴍᴜᴛᴇs ᴀ ᴜsᴇʀ.
-"""
-
-__mod_name__ = "Mᴜᴛᴇs"
