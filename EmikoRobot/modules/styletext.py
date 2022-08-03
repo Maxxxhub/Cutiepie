44ecd4b2683c33183668c3d1cb1b6fd21bcd135d
Suru <107604575+Suruxhub@users.noreply.github.com>
2022-08-03 16:49:12 +0530

    Update styletext.py

--- EmikoRobot/modules/styletext.py
+++ EmikoRobot/modules/styletext.py
@@ -473,7 +473,19 @@ def lined(update, context):
     else:
         message.reply_text(string)
 
-__mod_name__ = "Styletext"
+__mod_name__ = "Fᴏɴᴛ"
+
+__help__ = """
+*Sᴛʏʟɪsʜ ᴛᴇxᴛ*
+➻ /weebify <text>: Wᴇᴇʙɪғʏ ʏᴏᴜʀ ᴛᴇxᴛ!
+➻ /bubble <text>:Bᴜʙʙʟᴇ ʏᴏᴜʀ ᴛᴇxᴛ!
+➻ /fbubble <text>: Bᴜʙʙʟᴇ-ғɪʟʟᴇᴅ ʏᴏᴜʀ ᴛᴇxᴛ
+➻ /square <text>: Sǫᴜᴀʀᴇ ʏᴏᴜʀ ᴛᴇxᴛ!
+➻ /fsquare <text>: Sǫᴜᴀʀᴇ-ғɪʟʟᴇᴅ ʏᴏᴜʀ ᴛᴇxᴛ!
+➻ /blue <text>: Bʟᴜɪғʏ ʏᴏᴜʀ ᴛᴇxᴛ!
+➻ /latin <text>: Lᴀᴛɪɴɪғʏ ʏᴏᴜʀ ᴛᴇxᴛ
+➻ /lined <text>: Lɪɴᴇᴅ ʏᴏᴜʀ ᴛᴇxᴛ
+"""
 
 WEEBIFY_HANDLER = DisableAbleCommandHandler("weebify", weebify, run_async=True)
 BUBBLE_HANDLER = DisableAbleCommandHandler("bubble", bubble, run_async=True)
