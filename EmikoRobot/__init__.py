2511b156a057e2563ef88417913f0ddec73c8d8d
Suru <107604575+Suruxhub@users.noreply.github.com>
2022-08-04 21:06:10 +0530

    Update __init__.py

--- EmikoRobot/__init__.py
+++ EmikoRobot/__init__.py
@@ -205,6 +205,7 @@ else:
     STRING_SESSION = Config.STRING_SESSION
     LASTFM_API_KEY = Config.LASTFM_API_KEY
     CF_API_KEY = Config.CF_API_KEY
+    app = Client("app", bot_token=BOT_TOKEN, api_id=API_ID, api_hash=API_HASH)
 
     try:
         BL_CHATS = {int(x) for x in Config.BL_CHATS or []}
