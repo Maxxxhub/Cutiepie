76bd079c9f2974c7d80cbb17abd4c1a3ef52b5a6
Suru <107604575+Suruxhub@users.noreply.github.com>
2022-08-05 11:03:20 +0530

    Update shippering.py

--- EmikoRobot/modules/shippering.py
+++ EmikoRobot/modules/shippering.py
@@ -33,7 +33,7 @@ tomorrow = str(dt_tom())
 @capture_err
 async def couple(_, message):
     if message.chat.type == "private":
-        await message.reply_text("This command only works in groups.")
+        await message.reply_text("Tʜɪs ᴄᴏᴍᴍᴀɴᴅ ᴏɴʟʏ ᴡᴏʀᴋs ɪɴ ɢʀᴏᴜᴘs.!")
         return
     try:
         chat_id = message.chat.id
@@ -44,7 +44,7 @@ async def couple(_, message):
                 if not i.user.is_bot:
                     list_of_users.append(i.user.id)
             if len(list_of_users) < 2:
-                await message.reply_text("Not enough users")
+                await message.reply_text("ᴏᴘᴘs..! Nᴏᴛ ᴇɴᴏᴜɢʜ ᴜsᴇʀs ᴛᴏ ᴄʜᴏᴏsᴇ ᴀ ᴄᴏᴜᴘʟᴇ")
                 return
             c1_id = random.choice(list_of_users)
             c2_id = random.choice(list_of_users)
@@ -53,9 +53,9 @@ async def couple(_, message):
             c1_mention = (await app.get_users(c1_id)).mention
             c2_mention = (await app.get_users(c2_id)).mention
 
-            couple_selection_message = f"""**Couple of the day:**
-{c1_mention} + {c2_mention} = ❤️
-__New couple of the day may be chosen at 12AM {tomorrow}__"""
+            couple_selection_message = f"""**💝Cᴏᴜᴘʟᴇ ᴏғ ᴛʜᴇ ᴅᴀʏ💝:**
+{c1_mention} + {c2_mention} = ❤️💝💖💜💟
+__Nᴇᴡ ᴄᴏᴜᴘʟᴇ ᴏғ ᴛʜᴇ ᴅᴀʏ ᴍᴀʏ ʙᴇ ᴄʜᴏᴏsᴇɴ ᴀᴛ 12AM {tomorrow}__"""
             await app.send_message(message.chat.id, text=couple_selection_message)
             couple = {"c1_id": c1_id, "c2_id": c2_id}
             await save_couple(chat_id, today, couple)
@@ -65,9 +65,9 @@ __New couple of the day may be chosen at 12AM {tomorrow}__"""
             c2_id = int(is_selected["c2_id"])
             c1_name = (await app.get_users(c1_id)).first_name
             c2_name = (await app.get_users(c2_id)).first_name
-            couple_selection_message = f"""**Couple of the day:**
-[{c1_name}](tg://openmessage?user_id={c1_id}) + [{c2_name}](tg://openmessage?user_id={c2_id}) = ❤️
-__New couple of the day may be chosen at 12AM {tomorrow}__"""
+            couple_selection_message = f"""**💖Cᴏᴜᴘʟᴇ ᴏғ ᴛʜᴇ ᴅᴀʏ💖:**
+[{c1_name}](tg://openmessage?user_id={c1_id}) + [{c2_name}](tg://openmessage?user_id={c2_id}) = ❤️💜💝💗💖
+__Nᴇᴡ ᴄᴏᴜᴘʟᴇ ᴏғ ᴛʜᴇ ᴅᴀʏ ᴍᴀʏ ʙᴇ ᴄʜᴏᴏsᴇɴ ᴀᴛ 12AM {tomorrow}__"""
             await app.send_message(message.chat.id, text=couple_selection_message)
     except Exception as e:
         print(e)
