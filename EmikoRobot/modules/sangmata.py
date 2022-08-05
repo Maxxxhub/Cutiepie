5343bdf9c0d1a93b6c63111d8bfc7b6a5e55df37
Suru <107604575+Suruxhub@users.noreply.github.com>
2022-08-05 10:55:15 +0530

    Update sangmata.py

--- EmikoRobot/modules/sangmata.py
+++ EmikoRobot/modules/sangmata.py
@@ -9,20 +9,20 @@ from asyncio.exceptions import TimeoutError
 @register(pattern="^/check_name ?(.*)")
 async def lastname(steal):
     steal.pattern_match.group(1)
-    puki = await steal.reply("```Retrieving Such User Information..```")
+    puki = await steal.reply("```Rᴇᴛʀɪᴠɪɴɢ sᴜᴄʜ ᴜsᴇʀ ɪɴғᴏʀᴍᴀᴛɪᴏɴ..Pʟᴇᴀsᴇ ᴡᴀɪᴛ ❄️```")
     if steal.fwd_from:
         return
     if not steal.reply_to_msg_id:
-        await puki.edit("```Please Reply To User Message.```")
+        await puki.edit("```Pʟᴇᴀsᴇ ʀᴇᴘʟʏ ᴛᴏ ᴜsᴇʀ ᴍᴇssᴀɢᴇ ᴛᴏ ᴄʜᴇᴄᴋ ᴛʜᴇɪʀ ɴᴀᴍᴇ ʜɪsᴛᴏʀʏ ❗```")
         return
     message = await steal.get_reply_message()
     chat = "@SangMataInfo_bot"
     user_id = message.sender.id
     id = f"/search_id {user_id}"
     if message.sender.bot:
-        await puki.edit("```Reply To Real User's Message.```")
+        await puki.edit("```Rᴇᴘʟʏ ᴛᴏ ʀᴇᴀʟ ᴜsᴇʀ's ᴍᴇssᴀɢᴇ.!```")
         return
-    await puki.edit("```Please wait...```")
+    await puki.edit("```Pʟᴇᴀsᴇ ᴡᴀɪᴛ ❄️...```")
     try:
         async with ubot.conversation(chat) as conv:
             try:
@@ -31,7 +31,7 @@ async def lastname(steal):
                 response = await conv.get_response()
             except YouBlockedUserError:
                 await steal.reply(
-                    "```Error, report to @kenbotsupport```"
+                    "```Eʀʀᴏʀ, ʀᴇᴘᴏʀᴛ ᴛʜɪs ᴛᴏ @Teddysupport```"
                 )
                 return
             if r.text.startswith("Name"):
@@ -44,7 +44,7 @@ async def lastname(steal):
             if response.text.startswith("No records") or r.text.startswith(
                 "No records"
             ):
-                await puki.edit("```I Can't Find This User's Information, This User Has Never Changed His Name Before.```")
+                await puki.edit("```I ᴄᴀɴ'ᴛ ғɪɴᴅ ᴛʜɪs ᴜsᴇʀ's ɪɴғᴏʀᴍᴀᴛɪᴏɴ, Tʜɪs ᴜsᴇʀ ʜᴀs ɴᴇᴠᴇʀ ᴄʜᴀɴɢᴇᴅ ᴛʜᴇɪʀ ɴᴀᴍᴇ ʙᴇғᴏʀᴇ ❗.```")
                 await ubot.delete_messages(
                     conv.chat_id, [msg.id, r.id, response.id]
                 )
@@ -56,4 +56,4 @@ async def lastname(steal):
                 conv.chat_id, [msg.id, r.id, response.id, respond.id]
             )
     except TimeoutError:
-        return await puki.edit("`I'm Sick Sorry...`")
+        return await puki.edit("`I'ᴍ sɪᴄᴋ, sᴏʟʟʏ...🤐`")
