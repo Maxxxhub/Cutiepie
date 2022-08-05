4122e53036573e7574e43edd7d21293341a0cac9
Suru <107604575+Suruxhub@users.noreply.github.com>
2022-08-05 11:40:55 +0530

    Update antichannel.py

--- EmikoRobot/modules/antichannel.py
+++ EmikoRobot/modules/antichannel.py
@@ -17,15 +17,15 @@ def set_antichannel(update: Update, context: CallbackContext):
         s = args[0].lower()
         if s in ["yes", "on"]:
             enable_antichannel(chat.id)
-            message.reply_html("Enabled antichannel in {}".format(html.escape(chat.title)))
+            message.reply_html("Eɴᴀʙʟᴇᴅ ᴀɴᴛɪᴄʜᴀɴɴᴇʟ ɪɴ {} ✅".format(html.escape(chat.title)))
         elif s in ["off", "no"]:
             disable_antichannel(chat.id)
-            message.reply_html("Disabled antichannel in {}".format(html.escape(chat.title)))
+            message.reply_html("Dɪsᴀʙʟᴇᴅ ᴀɴᴛɪᴄʜᴀɴɴᴇʟ ɪɴ {} ❌".format(html.escape(chat.title)))
         else:
-            message.reply_text("Unrecognized arguments {}".format(s))
+            message.reply_text("Uɴʀᴇᴄᴏɢɴɪᴢᴇᴅ ᴀʀɢᴜᴍᴇɴᴛs {}".format(s))
         return
     message.reply_html(
-        "Antichannel setting is currently {} in {}".format(antichannel_status(chat.id), html.escape(chat.title)))
+        "Aɴᴛɪᴄʜᴀɴɴᴇʟ sᴇᴛᴛɪɴɢ ᴀʀᴇ ᴄᴜʀʀᴇɴᴛʟʏ sᴇᴛ ᴛᴏ {} ɪɴ {}".format(antichannel_status(chat.id), html.escape(chat.title)))
 
 
 @emikomsg(Filters.chat_type.groups, group=110)
@@ -39,3 +39,9 @@ def eliminate_channel(update: Update, context: CallbackContext):
         message.delete()
         sender_chat = message.sender_chat
         bot.ban_chat_sender_chat(sender_chat_id=sender_chat.id, chat_id=chat.id)
+
+__help__ = """
+➾ `/antichannel on` *:* Tᴜʀɴs ᴏɴ ᴀɴᴛɪᴄʜᴀɴɴᴇʟ ɪɴ ʏᴏᴜʀ ᴄʜᴀᴛ.
+➾ `/antichannel off` *:* Tᴜʀɴs ᴏғғ ᴀɴᴛɪᴄʜᴀɴɴᴇʟ ɪɴ ʏᴏᴜʀ ᴄʜᴀᴛ.
+"""
+__mod_name__ = "A-ᴄʜᴀɴɴᴇʟ"
