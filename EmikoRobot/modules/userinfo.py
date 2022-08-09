891ef987821ed66b74615d7bb987a4d035beddd4
Suru <107604575+Suruxhub@users.noreply.github.com>
2022-08-09 08:36:03 +0530

    Update userinfo.py

--- EmikoRobot/modules/userinfo.py
+++ EmikoRobot/modules/userinfo.py
@@ -445,9 +445,9 @@ def set_about_me(update: Update, context: CallbackContext):
 
 @sudo_plus
 def stats(update: Update, context: CallbackContext):
-    stats = "<b>╔═━「 ᴄᴜʀʀᴇɴᴛ ᴛᴇᴅᴅy ꜱᴛᴀᴛꜱ 」</b>\n" + "\n".join([mod.__stats__() for mod in STATS])
+    stats = "<b>╔═━「 ᴄᴜʀʀᴇɴᴛ ᴄᴜᴛɪᴇᴘɪᴇ ꜱᴛᴀᴛꜱ ツ 」</b>\n" + "\n".join([mod.__stats__() for mod in STATS])
     result = re.sub(r"(\d+)", r"<code>\1</code>", stats)
-    result += "\n<b>╘═━「 ᴩᴏᴡᴇʀᴇᴅ ʙy ᴛᴇᴅᴅy 」</b>"
+    result += "\n<b>╘═━「 ᴩᴏᴡᴇʀᴇᴅ ʙy Cᴜᴛɪᴇᴘɪᴇ ♛ 」</b>"
     update.effective_message.reply_text(
         result,
         parse_mode=ParseMode.HTML, 
